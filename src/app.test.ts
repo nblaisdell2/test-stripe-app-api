de7d3b739b70392ac4443fc40dca4bd80afee30f
Nicolas Blaisdell <nblaisdell2@gmail.com>
2023-08-01 10:41:09 -0400

    fixed test

--- src/app.test.ts
+++ src/app.test.ts
@@ -4,16 +4,13 @@ import app from "../src/app";
 describe("The Main API", () => {
   it("should return a status 200 & a Message", () => {
     return request(app)
-      .get("/")
+      .get("/status")
       .expect(200)
       .expect("Content-Type", /json/)
       .then((response) => {
-        console.log("body", response.body);
         expect(response.body).toEqual(
           expect.objectContaining({
-            customMessage: expect.objectContaining({
-              msg: "Hello",
-            }),
+            msg: "API is up and running",
           })
         );
       });
